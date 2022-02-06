5d31fa262576a9f19f691bbfe69911feafd27d13
MDBean <dat.doquoc@phenikaa-uni.edu.vn>
2022-02-06 13:54:02 +0700

    add 4 service base

--- src/flexisoft_source_node.cpp
+++ src/flexisoft_source_node.cpp
@@ -1,20 +1,55 @@
 #include <sick_flexisoft_pkg/flexisoft_library.h>
 #include <sick_flexisoft_pkg/flexisoft_define.h>
-#include <sick_flexisoft_pkg/FlexGetStopOperationalSrv.h>
+#include <sick_flexisoft_pkg/FlexSetStopOperationalSrv.h>
+#include <sick_flexisoft_pkg/FlexSetZoneSrv.h>
+#include <sick_flexisoft_pkg/FlexSetMuteReleaseSrv.h>
+#include <sick_flexisoft_pkg/FlexSetPayloadSrv.h>
+#include <sick_flexisoft_pkg/FlexSetMappingSrv.h>
 
 
 
 clientSock *Flexisoft = new clientSock("10.147.20.68", 1100);
 
-bool add(sick_flexisoft_pkg::FlexGetStopOperationalSrv::Request  &req,
-         sick_flexisoft_pkg::FlexGetStopOperationalSrv::Response &res)
+bool ServiceCbFlexSetStopOperationalSrv(sick_flexisoft_pkg::FlexSetStopOperationalSrv::Request  &req,
+                                        sick_flexisoft_pkg::FlexSetStopOperationalSrv::Response &res)
 {
   res.success = req.STOP_OPERATIONAL;
   
   ROS_INFO("sending back response: [%B]", res.success);
   return true;
 } 
-
+bool ServiceCbFlexSetZoneSrv(sick_flexisoft_pkg::FlexSetZoneSrv::Request  &req,
+                                        sick_flexisoft_pkg::FlexSetZoneSrv::Response &res)
+{
+  res.success = req.STOP_OPERATIONAL;
+  
+  ROS_INFO("sending back response: [%B]", res.success);
+  return true;
+}
+bool ServiceCbFlexSetMuteReleaseSrv(sick_flexisoft_pkg::FlexSetMuteReleaseSrv::Request  &req,
+                                        sick_flexisoft_pkg::FlexSetMuteReleaseSrv::Response &res)
+{
+  res.success = req.STOP_OPERATIONAL;
+  
+  ROS_INFO("sending back response: [%B]", res.success);
+  return true;
+}
+bool ServiceCbFlexSetPayloadSrv(sick_flexisoft_pkg::FlexSetPayloadSrv::Request  &req,
+                                        sick_flexisoft_pkg::FlexSetPayloadSrv::Response &res)
+{
+  res.success = req.STOP_OPERATIONAL;
+  
+  ROS_INFO("sending back response: [%B]", res.success);
+  return true;
+}
+bool ServiceCbFlexSetMappingSrv(sick_flexisoft_pkg::FlexSetMappingSrv::Request  &req,
+                                        sick_flexisoft_pkg::FlexSetMappingSrv::Response &res)
+{
+  res.success = req.STOP_OPERATIONAL;
+  
+  ROS_INFO("sending back response: [%B]", res.success);
+  return true;
+}
 
 
 int main(int argc, char **argv)
@@ -22,8 +57,15 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "safety_function");
     ros::NodeHandle nh;
-    ros::ServiceServer service = nh.advertiseService("FlexGetStopOperationalSrv", add);
-     ROS_INFO("Ready to add two ints.");
+
+    ros::ServiceServer Srv_FlexSetStopOperationalSrv = nh.advertiseService("FlexSetStopOperationalSrv", ServiceCbFlexSetStopOperationalSrv);
+    ros::ServiceServer Srv_FlexSetZoneSrv = nh.advertiseService("FlexSetZoneSrv", ServiceCbFlexSetZoneSrv);
+    ros::ServiceServer Srv_FlexSetMuteReleaseSrv = nh.advertiseService("FlexSetMuteReleaseSrv", ServiceCbFlexSetMuteReleaseSrv);
+    ros::ServiceServer Srv_FlexSetPayloadSrv = nh.advertiseService("FlexSetPayloadSrv", ServiceCbFlexSetPayloadSrv);
+    ros::ServiceServer Srv_FlexSetMappingSrv = nh.advertiseService("FlexSetMappingSrv", ServiceCbFlexSetMappingSrv);
+   
+
+
 
     //sick_flexisoft_pkg::SAFE_SP_AGV SAFE_SP_AGV;
     
@@ -66,3 +108,8 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+
+
+
+  
